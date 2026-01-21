437685d99b8e13f34eae2f73dd7d858442a07529
olfalafi <olfalafi1998@gmail.com>
2026-01-21 22:04:12 +0100

    add tailwindcss configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-<h1 className="text-4xl bg- font-bold">
-    Hello world!
-  </h1>    </>
-  )
+      <h1 className="bg- text-4xl font-bold">Hello world!</h1>{" "}
+    </>
+  );
 }
 
-export default App
+export default App;
