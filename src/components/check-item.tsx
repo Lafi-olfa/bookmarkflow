f2e4e322313814cec5fcefd0025bc31396bb13a1
olfalafi <olfalafi1998@gmail.com>
2026-05-05 22:25:54 +0100

    improve sidebar dark mode styling and extract CheckItem component

---
+++ src/components/check-item.tsx
@@ -0,0 +1,21 @@
+interface CheckItemsProps {
+  name: string;
+  count: string;
+}
+export default function Checktem({ name, count }: CheckItemsProps) {
+  return (
+    <div className="flex items-center justify-between px-2 py-1">
+      <div className="flex gap-2">
+        <input type="checkbox" name="" id="" />
+        <span className="text-base leading-[1.4] dark:text-neutral-100">
+          {name}
+        </span>
+      </div>
+      <div className="flex w-fit items-center rounded-full border border-neutral-200 px-2 py-0.5 dark:border-[#004241] dark:bg-[#002E2D]">
+        <p className="font-manrope w-fit text-xs leading-[1.4em] font-medium text-[#4c5c59] dark:text-[#FFF]">
+          {count}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/components/sibebar.tsx
+++ src/components/sibebar.tsx
@@ -5,16 +5,16 @@ import Close from "../assets/icon-close.svg";
 
 import LogoLight from "../assets/logo-light-theme.svg";
 import { useTheme } from "../context/theme-context";
+import Checktem from "./check-item";
 export default function Sidebar() {
   const { theme } = useTheme();
-  console.log(theme);
   return (
-    <div className="relative h-96 w-72 px-4 py-3 dark:bg-neutral-800">
+    <div className="relative h-96 w-72 px-4 py-3 dark:bg-[#002E2D]">
       <button className="absolute top-1 right-1 h-4 w-4">
         <img
           src={Close}
           alt="Menu"
-          className="h-5 w-5 text-neutral-900 dark:text-white"
+          className="h-5 w-5 dark:brightness-0 dark:contrast-100 dark:invert"
         />
       </button>
 
@@ -32,25 +32,19 @@ export default function Sidebar() {
           />
           <span>Home</span>
         </div>
-        <div className="flex items-center justify-start gap-2 px-2 py-1 text-neutral-800 dark:text-neutral-100">
-          <img src={Archive} alt="Menu" className="h-5 w-5" />
+        <div className="flex items-center justify-start gap-2 px-2 py-1 dark:text-neutral-100">
+          <img
+            src={Archive}
+            alt="Menu"
+            className="h-5 w-5 dark:brightness-0 dark:contrast-100 dark:invert"
+          />
           <span>Archived</span>
         </div>
       </div>
-      <span className="px-2 py-1 text-base leading-[1.4] text-neutral-800 dark:text-neutral-100">
+      <span className="px-2 py-1 text-base leading-[1.4] dark:text-neutral-100">
         TAGS
       </span>
-      <div className="flex items-center justify-between px-2 py-1">
-        <div className="flex gap-2">
-          <input type="checkbox" name="" id="" />
-          <span className="text-base leading-[1.4] text-neutral-800 dark:text-neutral-100">
-            Home
-          </span>
-        </div>
-        <span className="rounded-3xl border bg-gray-300 px-2 py-1 text-xs leading-[1.4] text-neutral-800 dark:border-neutral-300 dark:bg-neutral-600 dark:text-white">
-          1
-        </span>
-      </div>
+      <Checktem name="Home" count="3" />
     </div>
   );
 }
