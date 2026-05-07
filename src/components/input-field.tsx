7b38fb7c3cfe97782be6d87fff193fd3bfc14450
olfalafi <olfalafi1998@gmail.com>
2026-05-07 23:39:41 +0100

    Wip: Complet sign-in page

--- src/components/input-field.tsx
+++ src/components/input-field.tsx
@@ -8,9 +8,14 @@ export default function InputField({
   name: string;
 }) {
   return (
-    <div className="flex flex-col bg-red-500">
-      <span>{fieldName}</span>
-      <input type={type} name={name} id="" />
+    <div className="flex flex-col gap-0.5">
+      <span className="text-sm leading-[1.4] dark:text-white">{fieldName}</span>
+      <input
+        type={type}
+        name={name}
+        id=""
+        className="rounded-lg border p-2 shadow-sm dark:border-teal-500 dark:bg-teal-800"
+      />
     </div>
   );
 }
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -7,24 +7,42 @@ export default function SignIn() {
   const { theme } = useTheme();
 
   return (
-    <div className="flex min-h-screen min-w-screen flex-col items-center justify-center gap-2.5 overflow-hidden border border-dashed border-amber-500 bg-[#e8f0ef]">
-      <div className="radius-12 border border-amber-500 px-4 py-3">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-2.5 overflow-hidden bg-[#e8f0ef]">
+      <div className="rounded-2xl px-2 py-4 dark:bg-[#002E2D]">
+        {" "}
         <img
           src={`${theme === "dark" ? LogoDark : LogoLight}`}
           alt="Menu"
-          className="h-auto w-auto dark:text-white"
+          className="h-auto w-auto px-5"
         />
-        <div>
-          <h2>Log in to your account</h2>
-          <p>Welcome back! Please enter your details.</p>
+        <div className="px-5 py-8">
+          <p className="font-manrope text-2xl leading-[1.4] dark:text-white">
+            Log in to your account
+          </p>
+          <p className="font-manrope w-full text-sm font-medium tracking-[0.01em] text-[#4C5C59]">
+            Welcome back! Please enter your details.
+          </p>
         </div>
-        <form action="" method="post">
+        <form action="" method="post" className="flex flex-col gap-4 px-5">
           <InputField fieldName="Email" type="text" name="email" />
           <InputField fieldName="Password" type="text" name="password" />
-          <button>Log in</button>
+          <button className="rounded-lg bg-[#014745] p-2 text-base leading-[1.4] text-white shadow-sm dark:bg-teal-800">
+            Log in
+          </button>
         </form>
-        <div>
-          <div>Forgot password?</div>
+        <div className="flex flex-col items-center px-5 py-4">
+          <div className="flex gap-0.5 text-sm">
+            <span className="font-manrope leading-normal text-[#4C5C59] dark:text-neutral-100">
+              Forgot password?
+            </span>
+            <span className="leading-[1.4] dark:text-white">Reset it</span>
+          </div>
+          <div className="lea flex gap-0.5 text-sm">
+            <span className="font-manrope leading-normal text-[#4C5C59] dark:text-neutral-100">
+              Don’t have an account?
+            </span>
+            <span className="leading-[1.4] dark:text-white">Sign up</span>
+          </div>
         </div>
       </div>
     </div>
